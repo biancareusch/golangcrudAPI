c273c9c2cd27ab137f087ff08ba47bf1bdd0a204
Bianca Reusch <reusch.bianca@gmail.com>
2021-02-23 21:10:36 -0600

    still working on unit test

--- endpoints_test.go
+++ endpoints_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -20,7 +21,8 @@ func TestGetPeople(t *testing.T){
 
 	//Check the response body is what's expected
 	expected := `[{"ID":1,"firstName":"Bianca","lastName":"Reusch","Age":27}]`
-	if rr.Body.String() != expected {
+	bodyString := rr.Body.String()
+	if strings.Contains(bodyString, expected) {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
 	}
@@ -43,8 +45,26 @@ func TestGetPersonByID(t *testing.T){
 	}
 	//Check the response body is what's expected
 	expected := `{"id":1,"FirstName":"Bianca","LastName":"Reusch","Age":27}`
-	if rr.Body.String() != expected {
+	if strings.Contains(rr.Body.String(), expected) {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
+	}
+}
+
+
+func TestGetEntryByIDNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/showPerson", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	q.Add("id", "123")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(showPerson)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status == http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
 	}
 }
