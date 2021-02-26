a11a490770ef4f30ccd1392faed74413b75f5edf
Bianca Reusch <reusch.bianca@gmail.com>
2021-02-26 10:48:59 -0600

    added mock data and test for FindByID

--- repository/mysql/mysql_test.go
+++ repository/mysql/mysql_test.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
-	
+import (
+	"database/sql"
+	"log"
+	"strconv"
+	"testing"
+	"time"
+
+	r "github.com/moemoe89/go-unit-test-sql/repository"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+var u = &r.UserModel{
+	ID:          uuid.New().String(),
+	FirstName:   "Momo",
+	LastName:    "Mock",
+	Age:         35,
+	DateJoined:  time.Now(),
+	DateUpdated: time.Now(),
 }
+
+func NewMock() (*sql.DB, sqlmock.Sqlmock){
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection",err)
+
+	}
+	return db, mock
+}
+
+func TestFindByID(t *testing.T){
+	db,mock := NewMock()
+	repo := &repository{db}
+	defer func(){
+		repo.Close()
+	}()
+
+	query := "SELECT id, first_name,last_name, age,date_joined,date_updated FROM person  WHERE id = \\?"
+
+	rows := sqlmock.NewRows([]string{"id","first_name","last_name", "age","date_joined","date_updated"}).AddRow(u.ID, u.FirstName, u.LastName, u.Age, u.DateJoined, u.DateUpdated)
+
+	mock.ExpectQuery(query).WithArgs(u.ID).WillReturnRows(rows)
+
+	uId, _ := strconv.Atoi(u.ID)
+	user, err := repo.FindByID(uId)
+	assert.NotNil(t, user)
+	assert.NoError(t,err)
+}
+
